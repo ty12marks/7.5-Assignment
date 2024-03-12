1816a3ce24047bdab0a64dc0d171e40ffae49ef4
ty12marks <143560321+ty12marks@users.noreply.github.com>
2024-03-12 01:45:45 +0000

    Rick and Morty

--- main.js
+++ main.js
@@ -2,26 +2,32 @@
 
 document.addEventListener('alpine:init', () => {
   Alpine.data('example', () => ({
-    // ... (existing code)
-
-    // Function to send messages
-    sendMessages() {
-      this.ids.map((j) => {
-        // Use a function to generate a new message each time
-        const generatedMessage = () => this.isTemplate
-          ? dummyjson.parse(this.template)
-          : this.template;
-        this.sendD2CMessageCore(
-          this.clients[j],
-          generatedMessage(),
-          this.statuses[j],
-          this.totalStatus,
-        );
-      });
-      this.totalStatus.addSent(this.deviceCount);
+    // Placeholder data or initialization, modify as needed
+    character: {
+      id: 15,
+      name: "Alien Rick",
+      status: "unknown",
+      species: "Alien",
+      type: "",
+      gender: "Male",
+      origin: {
+        name: "unknown",
+        url: ""
+      },
+      location: {
+        name: "Citadel of Ricks",
+        url: "https://rickandmortyapi.com/api/location/3"
+      },
+      image: "https://rickandmortyapi.com/api/character/avatar/15.jpeg",
+      episode: ["https://rickandmortyapi.com/api/episode/10"],
+      url: "https://rickandmortyapi.com/api/character/15",
+      created: "2017-11-04T20:56:13.215Z"
     },
 
-    // ... (existing code)
+    // Function to log character data (you can modify this part)
+    logCharacterData() {
+      console.log(this.character);
+    },
   }));
 });
 
